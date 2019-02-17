ba9e0227975270ca5addc4127749923753e14ead
Ryan Wold <wold@afomi.com>
2019-02-16 23:23:43 -0800

    add rspec and stub a test

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Glean
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # For RSpec
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
